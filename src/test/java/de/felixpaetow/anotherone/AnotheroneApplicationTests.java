647570e282c190da414ab0f93ca227342bffafbf
Felix Paetow <fhmpaetow@fsfe.org>
2022-01-23 20:57:53 +0100

    Initial commit

---
+++ src/test/java/de/felixpaetow/anotherone/AnotheroneApplicationTests.java
@@ -0,0 +1,13 @@
+package de.felixpaetow.anotherone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotheroneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
